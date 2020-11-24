7b43b465248ab27b80d1087a847569fcdb90fd87
umairnawaz333 <umairnawaz333@users.noreply.github.com>
2020-11-24 15:33:13 +0500

    My complete BMI calculator.

--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -59,7 +59,7 @@ class ResultsPage extends StatelessWidget {
             ),
           ),
           BottomButton(
-            buttonTitle: 'RE-CALCULATE',
+            buttonTitle: 'Re CALCULATE',
             onTap: () {
               Navigator.pop(context);
             },
