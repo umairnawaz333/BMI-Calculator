52d20b610aba9fdb08da86bdf07bd3a7e50c8e23
umairnawaz333 <umairnawaz333@users.noreply.github.com>
2020-11-24 15:32:52 +0500

    My complete BMI calculator.

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -48,7 +48,7 @@ class _InputPageState extends State<InputPage> {
                       : kInactiveCardColour,
                   cardChild: IconContent(
                     icon: FontAwesomeIcons.mars,
-                    label: 'MALE',
+                    label: 'Male',
                   ),
                 ),
               ),
@@ -64,7 +64,7 @@ class _InputPageState extends State<InputPage> {
                       : kInactiveCardColour,
                   cardChild: IconContent(
                     icon: FontAwesomeIcons.venus,
-                    label: 'FEMALE',
+                    label: 'Female',
                   ),
                 ),
               ),
