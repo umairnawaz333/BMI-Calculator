b7fd29b4207721cfb4fc7057d756a763b12515f9
umairnawaz333 <umairnawaz333@users.noreply.github.com>
2020-11-25 15:21:28 +0500

    Code Complete Change for Marks...

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,52 @@
+import 'dart:async';
+import 'package:bmi_calculator/MainPage.dart';
 import 'package:flutter/material.dart';
-import 'package:bmi_calculator/screens/input_page.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'MainPage.dart';
 
-void main() => runApp(BMICalculator());
 
-class BMICalculator extends StatelessWidget {
+void main() => runApp(SplashScreen());
+
+class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Colors.indigoAccent
+      home: MySplashScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MySplashScreen extends StatefulWidget {
+  @override
+  _MySplashScreenState createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 5), ()=>Navigator.push(context, MaterialPageRoute(builder: (context) => BmiCalculatur())));
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('images/bmi1.png', height: 250.0, width: 250,),
+
+          SizedBox( height: 30.0),
+
+          SpinKitRipple(color: Colors.red, size: 150.0, borderWidth: 10.0,),
+        ],
       ),
-      home: InputPage(),
     );
   }
 }
