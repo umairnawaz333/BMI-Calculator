ade38e492b65137ac3b91e9e63262b9c6bd4cf32
umairnawaz333 <umairnawaz333@users.noreply.github.com>
2020-11-25 15:45:24 +0500

    Change Color For input and Round button..

--- lib/constent_variable.dart
+++ lib/constent_variable.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-const activationcolor = Color(0xFF111328);
+const activationcolor = Colors.blue;
 const deactivationcolor = Color(0xFF1D1E33);
 
 const ConstentTextLabel = TextStyle(
-  fontSize: 15,
+  fontSize: 14,
   fontWeight: FontWeight.bold,
 );
 
 const ConstentTextLabe2 = TextStyle(
-  fontSize: 60,
+  fontSize: 35,
   fontWeight: FontWeight.w900,
 );
 
